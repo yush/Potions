c3b6b199cb059f729c7d52b15d6ab8c0d2fe7541
Clément OURY <ourycl@gmail.com>
2016-11-12 09:39:07 +0100

    release

--- config/dev.exs
+++ config/dev.exs
@@ -37,7 +37,7 @@ config :phoenix, :stacktrace_depth, 20
 config :cookBook, CookBook.Repo,
   adapter: Ecto.Adapters.Postgres,
   username: "postgres",
-  password: "postgres",
-  database: "cookbook_dev",
+  password: "sushi",
+  database: "cookbook",
   hostname: "localhost",
   pool_size: 10
